783e624a5dc40f2b724df47793faa588d27fb8c4
Manh Nguyen <manhnd9293@gmail.com>
2025-06-01 22:52:31 +0700

    init project

---
+++ app.py
@@ -0,0 +1,49 @@
+import re
+from flask import Flask, render_template, request
+from search import Search
+
+es = Search()
+
+app = Flask(__name__)
+
+
+@app.get('/')
+def index():
+    return render_template('index.html')
+
+
+@app.post('/')
+def handle_search():
+    query = request.form.get('query', '')
+    from_ = request.form.get('from_', type=int, default=0)
+    results = es.search(
+        query={
+            'multi_match': {
+                'query': query,
+                'fields': ['name', 'summary', 'content'],
+            }
+        }, size = 5, from_=from_
+    )
+    
+    return render_template(
+        'index.html', 
+        query=query, 
+        results=results['hits']['hits'], 
+        from_=0, 
+        total=results['hits']['total']['value']
+        )
+
+
+@app.get('/document/<id>')
+def get_document(id):
+    document = es.retrieve_document(id)
+    title = document['_source']['name']
+    paragraphs = document['_source']['content'].split('\n')
+    return render_template('document.html', title=title, paragraphs=paragraphs)
+
+@app.cli.command()
+def reindex():
+    """Regenerate the Elasticsearch index."""
+    response = es.reindex()
+    print(f'Index with {len(response["items"])} documents created '
+          f'in {response["took"]} milliseconds.')
